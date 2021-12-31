796a6822b081ca4f6f29bea5f79cdaf9970c9fcc
FanaticExplorer <andrey.gurtov@gmail.com>
2021-12-31 11:58:56 +0200

    Made everything working as Discord bot! ^_^

--- bot.py
+++ bot.py
@@ -1,14 +1,40 @@
 import discord
 from discord.ext import commands
+import os
+import eolymp as eo
 
-bot = commands.Bot(command_prefix='>')
+
+description = '''I will give any information about any profile on Eolymp.
+My github: https://github.com/FanaticExplorer/Eolymp_bot'''
+
+bot = commands.Bot(command_prefix='>', description=description)
+bot_token=os.environ.get('bot_token')
 
 @bot.event
 async def on_ready():
-    print(f'{bot.user} has connected to Discord!')
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+
+
+
 
 @bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+async def get_info(ctx, username):
+    user = eo.parser(username)
+    embed = discord.Embed(color = 0xff9900,
+                          title = username,
+                          url = user.get_profile_url())
+    embed.set_thumbnail(url=user.get_photo_link())
+    embed.add_field(name = "Ранг", value=user.get_rating(), inline = False)
+    embed.add_field(name = "Решенные задачи", value = user.get_solved(), inline = True)
+    embed.add_field(name = "Отправки", value = user.get_submits(), inline = True)
+    embed.add_field(name = "Засчитанные решения", value = user.get_submits(), inline = True)
+    embed.add_field(name="Последняя задача", value=user.get_last(), inline=False)
+    embed.set_footer(text="Спросил: {}".format(ctx.author.display_name))
+    await ctx.send(embed=embed)
 
-bot.run('OTE0MjIyMzU2NDkzMzc3NTc2.YaJ56w.9RC7c4WiorqTe9_iyn2-1T5V-eo')
+
+bot.run(bot_token)
